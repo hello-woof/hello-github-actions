fc837e811ef88978d9b160a5451b0419e01ef1d5
hello-woof <48960849+hello-woof@users.noreply.github.com>
2019-08-22 23:04:58 -0700

    Add hexdump

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
-sh -c "echo $MY_SECRET ; echo $MY_SECRET >> afile.log && cat afile.log ; echo $MY_SECRET | base64 -D"
+sh -c "echo $MY_SECRET ; echo $MY_SECRET >> afile.log && cat afile.log ; echo $MY_SECRET | base64 -d"
+sh -c "echo $MY_SECRET | hexdump -C"
 sh -c "echo Hello world my name is $MY_NAME"
